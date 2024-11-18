29cdf0be7208a2c500dd6ef14f7b57b103fd12e0
Malo <malo.guivarch@student-cs.fr>
2024-11-18 09:15:03 +0100

    1er version du lundi matin

--- EasyPrescription/dashboard/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from pharmacies import models
-from dashboard import das
-
-# Create your views here.
-
-def index(request):
-    return render(request, 'dashboard/index.html')
