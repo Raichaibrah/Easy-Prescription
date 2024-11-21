a72ba7cbb7ee4b9001f2e4bf09ab112aa7dc03f7
Ibrah R Sanoussi Raïcha <raicha.ibrahsanoussi@student-cs.fr>
2024-11-21 22:18:50 +0100

    correction problème suppression et changements visuels pages

--- prescriptions/urls.py
+++ prescriptions/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('historique/', views.historique_ordonnances, name='historique_ordonnances'),
     path('ordonnance/<int:id>/supprimer/', views.supprimer_ordonnance, name='supprimer_ordonnance'),
-   path('soumettre/', views.soumettre_ordonnance, name='soumettre_ordonnance'),
+    path('soumettre/', views.soumettre_ordonnance, name='soumettre_ordonnance'),
 ]
